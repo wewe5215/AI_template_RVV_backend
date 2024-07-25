de481957de533cce918444a41cc500622b439dfd
wewe5215 <wewe5215@gmail.com>
2024-07-25 15:39:13 +0800

    feat:conv2d_add_relu with xnnpack

--- static/cpu/csrc/conv2d_bias_add_relu_0.cpp
+++ static/cpu/csrc/conv2d_bias_add_relu_0.cpp
@@ -64,35 +64,53 @@ void conv2d_bias_add_relu_0 (
   int i32_out_batch = *out_batch;
   int i32_out_h = *out_h;
   int i32_out_w = *out_w;
-  xnn_operator_t op = nullptr;
+  xnn_operator_t op_conv = nullptr;
   const xnn_status status_init = xnn_initialize(nullptr);
   CHECK_EQ(status_init, xnn_status_success);
+  // for conv2d_bias
   const xnn_status status = xnn_create_convolution2d_nhwc_f32(
     PH, PW, PH, PW, i32_kernel_h, i32_kernel_w,
     SH, SW, DH, DW, 1, CI,
     CO, 1 * CI, 1 * CO, (float*)(weight_ptr), (float*)(bias_ptr),
     0, std::numeric_limits<float>::infinity(),
-    /*flags=*/0, nullptr, nullptr, &op);
-  std::unique_ptr<xnn_operator, decltype(&xnn_delete_operator)> auto_op(op, xnn_delete_operator);
+    /*flags=*/0, nullptr, nullptr, &op_conv);
+  std::unique_ptr<xnn_operator, decltype(&xnn_delete_operator)> auto_op_conv(op_conv, xnn_delete_operator);
 
   CHECK_EQ(status, xnn_status_success);
-  CHECK_NE(op, nullptr);
+  CHECK_NE(op_conv, nullptr);
   size_t workspace_size = SIZE_MAX;
   size_t workspace_alignment = SIZE_MAX;
   CHECK_EQ(
     xnn_reshape_convolution2d_nhwc_f32(
-      op, i32_batch, i32_in_h, i32_in_w,
+      op_conv, i32_batch, i32_in_h, i32_in_w,
       &workspace_size, &workspace_alignment,
       /*output_height_out=*/nullptr, /*output_width_out=*/nullptr,
       /*threadpool=*/nullptr), xnn_status_success);
   CHECK_EQ(workspace_size, 0);
   CHECK_EQ(workspace_alignment, 1);
   CHECK_EQ(xnn_setup_convolution2d_nhwc_f32(
-      op, 
+      op_conv, 
       /*workspace=*/nullptr, 
       (float*)(in_ptr), 
       (float*)(out_ptr)), xnn_status_success);
 
-  CHECK_EQ(xnn_run_operator(op, /*threadpool=*/nullptr), xnn_status_success);
+  CHECK_EQ(xnn_run_operator(op_conv, /*threadpool=*/nullptr), xnn_status_success);
+  // for add
+  xnn_operator_t op_add = nullptr;
+  CHECK_EQ(xnn_status_success, xnn_create_add_nd_f32(0, std::numeric_limits<float>::infinity(), 0, &op_add));
+  std::unique_ptr<xnn_operator, decltype(&xnn_delete_operator)> auto_op_add(op_add, xnn_delete_operator);
+
+  const size_t a_shape[] = { (size_t)i32_out_batch, (size_t)i32_out_h, (size_t)i32_out_w, (size_t)i32_out_ch};
+  const size_t b_shape[] = { (size_t)i32_out_batch, (size_t)i32_out_h, (size_t)i32_out_w, (size_t)i32_out_ch};
+  CHECK_EQ(
+    xnn_status_success, xnn_reshape_add_nd_f32(
+                          op_add, 4, a_shape, 4, b_shape,
+                          /*threadpool=*/nullptr));
+
+  CHECK_EQ(
+    xnn_status_success, xnn_setup_add_nd_f32(op_add, (float*)(res_ptr), (float*)(out_ptr), (float*)(out_ptr)));
+
+  CHECK_EQ(xnn_status_success, xnn_run_operator(op_add, /*threadpool=*/nullptr));
+
   return;
 }
