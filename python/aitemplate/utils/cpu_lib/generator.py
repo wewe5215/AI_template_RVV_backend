59649b6fb90302101c28151865c472db25bdb65f
wewe5215 <wewe5215@gmail.com>
2024-07-31 15:41:37 +0800

    feat:operator generation for Conv2d, Conv2dBias, Conv2dBiasRelu, Conv2dBiasAdd, Conv2dBiasReluAdd, Conv2dBiasAddRelu

--- python/aitemplate/utils/cpu_lib/generator.py
+++ python/aitemplate/utils/cpu_lib/generator.py
@@ -26,18 +26,8 @@ from aitemplate.utils.cpu_lib import (
 
 
 ###########################################################################################################
-# Convolution for 2D Fwd operations
+# Convolution for 2D Fwd operations (Layout : NHWC)
 def CreateConv2dFwdOperator(manifest, operation_kind, out_element_op, out_data_op=""):
-    a_element_desc = library.TensorDesc(
-        library.DataType.f16, library.LayoutType.NHWC
-    )
-    b_element_desc = library.TensorDesc(
-        library.DataType.f16, library.LayoutType.NHWC
-    )
-    c_element_desc = library.TensorDesc(
-        library.DataType.f16, library.LayoutType.NHWC
-    )
-
     in_element_op = library.TensorOperation.PassThrough
     conv2d_specialization = [
         conv.Conv2DSpecialization.ConvNhwcF32,
@@ -46,7 +36,38 @@ def CreateConv2dFwdOperator(manifest, operation_kind, out_element_op, out_data_o
 
     operations = []
     for conv2d_spec in conv2d_specialization:
-            new_operation = conv.Conv2DOperation(
+            if conv2d_spec == conv.Conv2DSpecialization.ConvNhwcF32:
+                a_element_desc = library.TensorDesc(
+                    library.DataType.f32, library.LayoutType.NHWC
+                )
+                b_element_desc = library.TensorDesc(
+                    library.DataType.f32, library.LayoutType.NHWC
+                )
+                c_element_desc = library.TensorDesc(
+                    library.DataType.f32, library.LayoutType.NHWC
+                )
+                new_operation = conv.Conv2DOperation(
+                    operation_kind=operation_kind,
+                    extra_kind=out_element_op,
+                    A=a_element_desc,
+                    B=b_element_desc,
+                    C=c_element_desc,
+                    a_elem_op=in_element_op,
+                    b_elem_op=in_element_op,
+                    epilogue_functor=out_element_op,
+                    conv2d_specialization=conv2d_spec,
+                )
+            else:
+                a_element_desc = library.TensorDesc(
+                    library.DataType.f16, library.LayoutType.NHWC
+                )
+                b_element_desc = library.TensorDesc(
+                    library.DataType.f16, library.LayoutType.NHWC
+                )
+                c_element_desc = library.TensorDesc(
+                    library.DataType.f16, library.LayoutType.NHWC
+                )
+                new_operation = conv.Conv2DOperation(
                 operation_kind=operation_kind,
                 extra_kind=out_element_op,
                 A=a_element_desc,
@@ -63,120 +84,49 @@ def CreateConv2dFwdOperator(manifest, operation_kind, out_element_op, out_data_o
     return operations
 
 
-# # Convolution for 2D Bwd operations
-# def CreateConv2dBwdOperator(manifest, operation_kind, out_element_op, out_data_op=""):
-#     a_element_desc = library.TensorDesc(library.DataType.f16, library.LayoutType.GNWC)
-#     b_element_desc = library.TensorDesc(library.DataType.f16, library.LayoutType.GKXC)
-#     c_element_desc = library.TensorDesc(library.DataType.f16, library.LayoutType.GNWK)
-
-#     in_element_op = library.TensorOperation.PassThrough
-
-#     conv2d_specialization = [
-#         conv.Conv2DSpecialization.ConvBwdDataDefault,
-#         conv.Conv2DSpecialization.ConvBwd1x1S1P0,
-#     ]
-#     gemm_spec = conv.Conv2DSpecialization.GemmDefault
-
-#     operations = []
-#     for conv2d_spec in conv2d_specialization:
-#             new_operation = conv.Conv2DOperation(
-#                 operation_kind=operation_kind,
-#                 extra_kind=out_element_op,
-#                 A=a_element_desc,
-#                 B=b_element_desc,
-#                 C=c_element_desc,
-#                 a_elem_op=in_element_op,
-#                 b_elem_op=in_element_op,
-#                 epilogue_functor=out_element_op,
-#                 c_data_op=out_data_op,
-#                 conv2d_specialization=conv2d_spec,
-#                 gemm_specialization=gemm_spec,
-#             )
-#             manifest.append(new_operation)
-#             operations.append(new_operation)
-#     return operations
-
-
-# # Convolution for 2D Bwd + Bias operations
-# def CreateConv2dBwdBiasOperator(
-#     manifest, operation_kind, out_element_op, out_data_op=""
-# ):
-#     a_element_desc = library.TensorDesc(library.DataType.f16, library.LayoutType.GNHWK)
-#     b_element_desc = library.TensorDesc(library.DataType.f16, library.LayoutType.GKYXC)
-#     c_element_desc = library.TensorDesc(library.DataType.f16, library.LayoutType.GNHWC)
-
-#     in_element_op = library.TensorOperation.PassThrough
-
-#     conv2d_specialization = [
-#         conv.Conv2DSpecialization.ConvBwdDataDefault,
-#         conv.Conv2DSpecialization.ConvBwd1x1S1P0,
-#     ]
-#     gemm_spec = conv.Conv2DSpecialization.GemmDefault
-
-#     operations = []
-#     for conv2d_spec in conv2d_specialization:
-#             new_operation = conv.Conv2DOperation(
-#                 operation_kind=operation_kind,
-#                 extra_kind=out_element_op,
-#                 A=a_element_desc,
-#                 B=b_element_desc,
-#                 C=c_element_desc,
-#                 a_elem_op=in_element_op,
-#                 b_elem_op=in_element_op,
-#                 epilogue_functor=out_element_op,
-#                 c_data_op=out_data_op,
-#                 conv2d_specialization=conv2d_spec,
-#                 gemm_specialization=gemm_spec
-#             )
-#             manifest.append(new_operation)
-#             operations.append(new_operation)
-#     return operations
-
-
 
 def GenerateTensorOp(manifest):
     # Conv2d
     CreateConv2dFwdOperator(
         manifest,
-        library.Conv2dKind.GroupConv2dBiasRelu,
+        library.Conv2dKind.Conv2d,
         library.TensorOperation.PassThrough,
     )
     # Conv2dBias
-    # CreateConv2dFwdOperator(
-    #     manifest,
-    #     library.Conv2dKind.GroupConv2dBiasRelu,
-    #     library.TensorOperation.Add,
-    # )
-    # # Conv2dBiasRelu
-    # CreateConv2dFwdOperator(
-    #     manifest,
-    #     library.Conv2dKind.GroupConv2dBiasRelu,
-    #     library.TensorOperation.AddRelu,
-    # )
-    # # Conv2dBiasAdd
-    # CreateConv2dFwdOperator(
-    #     manifest,
-    #     library.Conv2dKind.GroupConv2dBiasRelu,
-    #     library.TensorOperation.AddAdd,
-    # )
-    # # Conv2dBiasReluAdd
-    # CreateConv2dFwdOperator(
-    #     manifest,
-    #     library.Conv2dKind.GroupConv2dBiasRelu,
-    #     library.TensorOperation.AddReluAdd,
-    # )
+    CreateConv2dFwdOperator(
+        manifest,
+        library.Conv2dKind.Conv2dBias,
+        library.TensorOperation.PassThrough,
+    )
+    # Conv2dBiasRelu
+    CreateConv2dFwdOperator(
+        manifest,
+        library.Conv2dKind.Conv2dBiasRelu,
+        library.TensorOperation.PassThrough,
+    )
+    # Conv2dBiasAdd
+    CreateConv2dFwdOperator(
+        manifest,
+        library.Conv2dKind.Conv2dBias,
+        library.TensorOperation.Add,
+    )
+    # Conv2dBiasReluAdd
+    CreateConv2dFwdOperator(
+        manifest,
+        library.Conv2dKind.Conv2dBiasReluAdd,
+        library.TensorOperation.Add,
+    )
     # # Conv2dBiasAddRelu
-    # CreateConv2dFwdOperator(
-    #     manifest,
-    #     library.Conv2dKind.GroupConv2dBiasRelu,
-    #     library.TensorOperation.AddAddRelu,
-    # )
+    CreateConv2dFwdOperator(
+        manifest,
+        library.Conv2dKind.Conv2dBiasAddRelu,
+        library.TensorOperation.Add,
+    )
     # # Conv2dBiasSigmoid
     # CreateConv2dFwdOperator(
     #     manifest,
     #     library.Conv2dKind.GroupConv2dBiasRelu,
     #     library.TensorOperation.AddSigmoid,
-    #     library.MemoryDataOperation.MemorySet,
     # )
     # # TransposedConv2d
     # CreateConv2dBwdOperator(
